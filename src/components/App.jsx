fb04845dd4d66a88df6a1ab91eeb1644ba1da340
Bruno Gomes <brunogomes1198@gmail.com>
2023-03-07 17:20:00 +1100

    Extract the contact card as a reusable Card component and use props to render the default contact card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <img
+          className="circle-img"
+          src={props.img}
+          alt="avatar_img"
+        />
+      </div>
+      <div className="bottom">
+        <p className="info">{props.tel}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
+      <Card 
+        name="Beyonce" 
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
     </div>
   );
 }

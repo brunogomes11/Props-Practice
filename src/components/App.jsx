ed83f988348f637150a63703e69e15cdd32a694d
Bruno Gomes <brunogomes1198@gmail.com>
2023-03-07 17:33:46 +1100

    Import the contacts.js file to create card components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,28 @@
 import React from "react";
-
-function Card(props) {
-  return (
-    <div className="card">
-      <div className="top">
-        <h2 className="name">{props.name}</h2>
-        <img
-          className="circle-img"
-          src={props.img}
-          alt="avatar_img"
-        />
-      </div>
-      <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
-      </div>
-    </div>
-  )
-}
+import Card from "./Card"
+import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card 
-        name="Beyonce" 
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        tel="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name} 
+        img={contacts[0].img}
+        tel={contacts[0].tel}
+        email={contacts[0].email}
+      />
+      <Card 
+        name={contacts[1].name} 
+        img={contacts[1].img}
+        tel={contacts[1].tel}
+        email={contacts[1].email}
+      />
+      <Card 
+        name={contacts[2].name} 
+        img={contacts[2].img}
+        tel={contacts[2].tel}
+        email={contacts[2].email}
       />
     </div>
   );

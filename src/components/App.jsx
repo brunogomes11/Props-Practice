203eb9ba7744b418e6ffc395a8d068a7b4f10ead
Bruno Gomes <brunogomes1198@gmail.com>
2023-03-07 19:50:41 +1100

    Add Avatar and Detail component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import Card from "./Card"
+import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card 
-        name={contacts[0].name} 
-        img={contacts[0].img}
-        tel={contacts[0].tel}
+      <Avatar img="https://media.licdn.com/dms/image/D5603AQHcuHh-8FF1nQ/profile-displayphoto-shrink_800_800/0/1676275647403?e=1683763200&v=beta&t=yMwxqLdxf236ZVXE4BE6xXRrwAHqZxjRgwQIJstmhUw"/>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
         email={contacts[0].email}
       />
-      <Card 
-        name={contacts[1].name} 
-        img={contacts[1].img}
-        tel={contacts[1].tel}
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
         email={contacts[1].email}
       />
-      <Card 
-        name={contacts[2].name} 
-        img={contacts[2].img}
-        tel={contacts[2].tel}
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
         email={contacts[2].email}
       />
     </div>
